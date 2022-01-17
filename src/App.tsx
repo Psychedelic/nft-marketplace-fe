8d27b8fc6d5f57100d4a7648bff38528861b01b2
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-01-17 15:52:26 +0530

    config: updated eslint and prettier configurations

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          NFT Marketplace UI
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        NFT Marketplace UI
+      </p>
+    </header>
+  </div>
+);
 
 export default App;
