b591373167095dea8bc7f06ad8b86172544d1b85
prasanth kumar lalapeta <40259256+prasanthLalapeta@users.noreply.github.com>
2022-01-18 11:17:09 +0530

    config: Added react i18next configuration to support internationalization (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        NFT Marketplace UI
-      </p>
-    </header>
-  </div>
-);
+const App = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{t('translation:common.title')}</p>
+      </header>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/react-i18next.d.ts
@@ -0,0 +1,13 @@
+import 'react-i18next';
+import en from './locales/en/translation.json';
+
+const resources = {
+  en: { translation: en },
+};
+
+declare module 'react-i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'common';
+    resources: typeof resources['en'];
+  }
+}
