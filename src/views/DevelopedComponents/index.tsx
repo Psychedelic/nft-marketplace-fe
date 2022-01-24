30d96799bee2879278ec33af7abb988c52b25946
prasanth kumar lalapeta <40259256+prasanthLalapeta@users.noreply.github.com>
2022-01-24 14:59:51 +0530

    feat: 🎸 Add reusable chips UI component required across the app (#8)
    * feat: Added codebase for chips component with different variations required
    
    * feat: Added filtered count chip component along with props validation
    
    * feat: Added filtered trait chip component along with props validation
    
    * feat: Added NFT traits chip component along with props validation

--- src/views/DevelopedComponents/index.tsx
+++ src/views/DevelopedComponents/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Container, Title, ComponentWrapper, ComponentTitle } from './styles';
+import {
+  Container,
+  Title,
+  ComponentWrapper,
+  ComponentTitle,
+  ComponentsList,
+} from './styles';
 import {
   ActionButton,
   SearchInput,
   FilterInput,
   ModalInput,
+  FilteredCountChip,
+  FilteredTraitsChip,
+  NFTTraitsChip,
 } from '../../components';
 
 const DevelopedComponents = () => {
@@ -35,6 +44,63 @@ const DevelopedComponents = () => {
         <ModalInput
           placeholder={t('translation:inputField.placeholder.amount')}
         />
+      </ComponentWrapper>
+      <ComponentWrapper>
+        <ComponentTitle>FilteredCountChip</ComponentTitle>
+        <ComponentsList>
+          <FilteredCountChip
+            label={t('translation:chips.labels.itemsLabel')}
+            count="10.0k"
+            showLogo={false}
+          />
+          <FilteredCountChip
+            label={t('translation:chips.labels.OwnersLabel')}
+            count="5.9k"
+            showLogo={false}
+          />
+          <FilteredCountChip
+            label={t('translation:chips.labels.FloorPriceLabel')}
+            count="22.12"
+            showLogo
+          />
+        </ComponentsList>
+      </ComponentWrapper>
+      <ComponentWrapper>
+        <ComponentTitle>FilteredTraitsChip</ComponentTitle>
+        <ComponentsList>
+          <FilteredTraitsChip
+            name="Red"
+            rim="Big Gem"
+            removeFilter={() => {
+              // eslint-disable-next-line no-console
+              console.log('callback');
+            }}
+          />
+          <FilteredTraitsChip
+            name="Crystal"
+            rim="Big Gem"
+            removeFilter={() => {
+              // eslint-disable-next-line no-console
+              console.log('callback');
+            }}
+          />
+          <FilteredTraitsChip
+            name="Psychedelic"
+            rim="Rim"
+            removeFilter={() => {
+              // eslint-disable-next-line no-console
+              console.log('callback');
+            }}
+          />
+        </ComponentsList>
+      </ComponentWrapper>
+      <ComponentWrapper>
+        <ComponentTitle>NFTTraitsChip</ComponentTitle>
+        <ComponentsList>
+          <NFTTraitsChip name="Crystal" rimValue="420 (4.20%)" />
+          <NFTTraitsChip name="Crystal" rimValue="420 (4.20%)" />
+          <NFTTraitsChip name="Crystal" rimValue="420 (4.20%)" />
+        </ComponentsList>
       </ComponentWrapper>
     </Container>
   );
