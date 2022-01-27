d5501286af5c257fc8bc6c1454d8ff34a3b7519f
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-01-27 13:30:34 +0530

    feat: Added activity table layout

--- src/components/tables/activity-table.tsx
+++ src/components/tables/activity-table.tsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { TableLayout } from './table-layout';
+import { mockTableData } from './mock-data';
 import { Container } from './styles';
+
+export interface rowProps {
+  item: {
+    name: string;
+  };
+  type: string;
+  price: string;
+  quantity: string;
+  from: string;
+  to: string;
+  time: string;
+}
 
 export const ActivityTable = () => {
   const { t } = useTranslation();
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: t('translation:tables.titles.item'),
+        accessor: ({ item }: rowProps) => <div>{item.name}</div>,
+      },
+      {
+        Header: t('translation:tables.titles.type'),
+        accessor: ({ type }: rowProps) => <div>{type}</div>,
+      },
+      {
+        Header: t('translation:tables.titles.price'),
+        accessor: ({ price }: rowProps) => <div>{price}</div>,
+      },
+      {
+        Header: t('translation:tables.titles.quantity'),
+        accessor: ({ quantity }: rowProps) => <div>{quantity}</div>,
+      },
+      {
+        Header: t('translation:tables.titles.from'),
+        accessor: ({ from }: rowProps) => <div>{from}</div>,
+      },
+      {
+        Header: t('translation:tables.titles.to'),
+        accessor: ({ to }: rowProps) => <div>{to}</div>,
+      },
+      {
+        Header: t('translation:tables.titles.time'),
+        accessor: ({ time }: rowProps) => <div>{time}</div>,
+      },
+    ],
+    [], // eslint-disable-line react-hooks/exhaustive-deps
+  );
+
   return (
     <Container>
-      <p>{t('translation:tables.placeholder')}</p>
+      <TableLayout columns={columns} data={mockTableData} />
     </Container>
   );
 };
