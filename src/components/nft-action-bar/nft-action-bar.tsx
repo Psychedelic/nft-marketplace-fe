7dd83fad411b4dcc92d9678a070b6cf58097804a
OyinloluB <oyinloluwab@gmail.com>
2022-02-03 02:54:47 +0100

    refactor: style changes made

--- src/components/nft-action-bar/nft-action-bar.tsx
+++ src/components/nft-action-bar/nft-action-bar.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ActionButton } from '../core';
 
-import { NftActionBarWrapper } from './styles';
+import {
+  NftActionBarWrapper,
+  ActionText,
+  ButtonWrapper,
+} from './styles';
 import back from '../../assets/back.svg';
 
 const NftActionBar = () => {
@@ -10,23 +14,22 @@ const NftActionBar = () => {
 
   return (
     <NftActionBarWrapper>
-      <p>
+      <ActionText>
         <img src={back} alt="back to results" />
         {t('translation:buttons.action.backToResults')}
-      </p>
-      <div>
+      </ActionText>
+      <ButtonWrapper>
         <ActionButton
           outline="light"
           text={`${t('translation:buttons.action.cancelListing')}`}
           type="outline"
         />
-        &nbsp;
         <ActionButton
           background="filled"
           text={`${t('translation:buttons.action.changePrice')}`}
           type="primary"
         />
-      </div>
+      </ButtonWrapper>
     </NftActionBarWrapper>
   );
 };
--- src/components/nft-action-bar/styles.ts
+++ src/components/nft-action-bar/styles.ts
@@ -4,24 +4,28 @@ export const NftActionBarWrapper = styled('div', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
+});
 
-  '& p': {
-    fontStyle: 'normal',
-    fontWeight: '500',
-    fontSize: '16px',
-    lineHeight: '19px',
-    color: '#777E90',
-    display: 'flex',
-    margin: '0',
+export const ActionText = styled('p', {
+  fontStyle: 'normal',
+  fontWeight: '500',
+  fontSize: '16px',
+  lineHeight: '19px',
+  color: '#777E90',
+  display: 'flex',
+  margin: '0',
 
-    '& img': {
-      marginRight: '5px',
-    },
+  '& img': {
+    marginRight: '5px',
   },
+});
 
-  '& div': {
-    display: 'flex',
-    alignItems: 'center',
-    width: '25%',
+export const ButtonWrapper = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  width: '25%',
+
+  '& button:first-child': {
+    marginRight: '5px',
   },
 });
