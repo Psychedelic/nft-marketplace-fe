b322b1002eb6c98a03be76ba939517c0814f7df7
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-02-07 12:40:12 +0530

    feat: Added NFT view router along with NFT view component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { darkTheme } from './stitches.config';
 import CollectionView from './views/CollectionView';
+import NFTView from './views/NFTView';
 import DevelopedComponents from './views/DevelopedComponents';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CollectionView />} />
+        <Route path="/nft/:nftId" element={<NFTView />} />
         <Route path="/components" element={<DevelopedComponents />} />
       </Routes>
     </BrowserRouter>
---
+++ src/views/NFTView/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+/* --------------------------------------------------------------------------
+ * NFT View Component
+ * --------------------------------------------------------------------------*/
+
+const NFTView = () => <Container>NFT View</Container>;
+
+export default NFTView;
