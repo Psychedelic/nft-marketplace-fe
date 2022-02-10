a43a07869d8df1fddf7e309a314d4b0e5f889daf
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-02-10 09:58:28 +0530

    refactor: updated NFT list grid styles to match Figma designs

--- src/components/nft-skeleton-list/styles.ts
+++ src/components/nft-skeleton-list/styles.ts
@@ -2,7 +2,7 @@ import { styled } from '../../stitches.config';
 
 export const ListWrapper = styled('div', {
   display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fill, minmax(210px, 1fr))',
+  gridTemplateColumns: 'repeat(auto-fill,minmax(185px, 1fr))',
   gridAutoFlow: 'row dense',
-  gridGap: '35px 57px',
+  gridGap: '35px 50px',
 });
