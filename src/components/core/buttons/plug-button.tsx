d5033e36b3137a65ba3652654769caaa7e01bb44
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-02-17 13:02:27 +0530

    refactor: added onclick callback to plug button

--- src/components/core/buttons/plug-button.tsx
+++ src/components/core/buttons/plug-button.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { PlugButtonContainer, PlugButtonText } from './styles';
 
+export type PlugButtonProps = {
+  handleClick: () => void;
+};
+
 /* --------------------------------------------------------------------------
  * Plug Button Component
  * --------------------------------------------------------------------------*/
 
-export const PlugButton = () => {
+export const PlugButton = ({ handleClick }: PlugButtonProps) => {
   const { t } = useTranslation();
 
   return (
-    <PlugButtonContainer>
+    <PlugButtonContainer onClick={handleClick}>
       <PlugButtonText>
         {t('translation:buttons.action.connectToPlug')}
       </PlugButtonText>
