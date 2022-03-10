c010c007ee3c9786c6bf35c5ffb6c731ec14f6b0
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-03-10 13:36:27 +0530

    chore: added infinite scroll configuration in NFT list component

--- src/components/nft-list/nft-list.tsx
+++ src/components/nft-list/nft-list.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 import { NftCard } from '../core/cards/nft-card';
+import { NftSkeletonList } from '../nft-skeleton-list';
 import { ListWrapper } from './styles';
 
 import { useNFTSStore } from '../../store';
@@ -9,9 +11,17 @@ export const NftList = () => {
 
   return (
     <ListWrapper>
-      {loadedNFTS.map((nft) => (
-        <NftCard data={nft} key={nft.id} />
-      ))}
+      <InfiniteScroll
+        pageStart={0}
+        hasMore={true || false}
+        loader={<NftSkeletonList />}
+        useWindow={false}
+        className="infinite-loader"
+      >
+        {loadedNFTS.map((nft) => (
+          <NftCard data={nft} key={nft.id} />
+        ))}
+      </InfiniteScroll>
     </ListWrapper>
   );
 };
--- src/components/nft-list/styles.ts
+++ src/components/nft-list/styles.ts
@@ -1,8 +1,10 @@
 import { styled } from '../../stitches.config';
 
 export const ListWrapper = styled('div', {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fill,minmax(185px, 1fr))',
-  gridAutoFlow: 'row dense',
-  gridGap: '35px 50px',
+  '.infinite-loader': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill,minmax(185px, 1fr))',
+    gridAutoFlow: 'row dense',
+    gridGap: '35px 50px',
+  },
 });
