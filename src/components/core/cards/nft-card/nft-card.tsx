bd65922738a636fd1ddcc44f7cec3674e77f4f40
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-03-11 13:16:03 +0530

    chore: memoize NFT card component

--- src/components/core/cards/nft-card/nft-card.tsx
+++ src/components/core/cards/nft-card/nft-card.tsx
@@ -25,67 +25,69 @@ export type NftCardProps = {
   displayVideo?: boolean;
 };
 
-export const NftCard = ({
-  owned,
-  notForSale,
-  forSaleAndOffer,
-  data,
-  displayVideo,
-}: NftCardProps) => {
-  const { t } = useTranslation();
+export const NftCard = React.memo(
+  ({
+    owned,
+    notForSale,
+    forSaleAndOffer,
+    data,
+    displayVideo,
+  }: NftCardProps) => {
+    const { t } = useTranslation();
 
-  return (
-    <RouterLink to={`/nft/${data.id}`}>
-      <CardWrapper>
-        <Flex>
-          <OwnedCardText>
-            {owned ? `${t('translation:nftCard.owned')}` : ''}
-          </OwnedCardText>
-          <CardOptionsDropdown />
-        </Flex>
-        <PreviewDetails>
-          {data.preview && !displayVideo ? (
-            <img src={data?.preview} alt="nft-card" />
-          ) : (
-            <video
-              data-icon-video
-              loop
-              autoPlay
-              muted
-              style={{
-                width: '100%',
-                minHeight: '125px',
-                borderRadius: '14px',
-              }}
-            >
-              <source src={data.location} type="video/mp4" />
-            </video>
-          )}
-        </PreviewDetails>
-        <Flex>
-          <NftName>{data?.name}</NftName>
-          {notForSale ? (
-            ''
-          ) : (
-            <Dfinity>
-              {data?.price}
-              <img src={wicpLogo} alt="" />
-            </Dfinity>
-          )}
-        </Flex>
+    return (
+      <RouterLink to={`/nft/${data.id}`}>
+        <CardWrapper>
+          <Flex>
+            <OwnedCardText>
+              {owned ? `${t('translation:nftCard.owned')}` : ''}
+            </OwnedCardText>
+            <CardOptionsDropdown />
+          </Flex>
+          <PreviewDetails>
+            {data.preview && !displayVideo ? (
+              <img src={data?.preview} alt="nft-card" />
+            ) : (
+              <video
+                data-icon-video
+                loop
+                autoPlay
+                muted
+                style={{
+                  width: '100%',
+                  minHeight: '125px',
+                  borderRadius: '14px',
+                }}
+              >
+                <source src={data.location} type="video/mp4" />
+              </video>
+            )}
+          </PreviewDetails>
+          <Flex>
+            <NftName>{data?.name}</NftName>
+            {notForSale ? (
+              ''
+            ) : (
+              <Dfinity>
+                {data?.price}
+                <img src={wicpLogo} alt="" />
+              </Dfinity>
+            )}
+          </Flex>
 
-        <Flex>
-          <NftId>{data?.id}</NftId>
-          {notForSale ? (
-            ''
-          ) : (
-            <LastOffer>
-              {forSaleAndOffer ? 'Offer for ' : 'Last '}
-              <b>{data?.lastOffer}</b>
-            </LastOffer>
-          )}
-        </Flex>
-      </CardWrapper>
-    </RouterLink>
-  );
-};
+          <Flex>
+            <NftId>{data?.id}</NftId>
+            {notForSale ? (
+              ''
+            ) : (
+              <LastOffer>
+                {forSaleAndOffer ? 'Offer for ' : 'Last '}
+                <b>{data?.lastOffer}</b>
+              </LastOffer>
+            )}
+          </Flex>
+        </CardWrapper>
+      </RouterLink>
+    );
+  },
+);
