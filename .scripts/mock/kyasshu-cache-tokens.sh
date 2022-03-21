14b30c995c8365191fe7de49d6ebe8817b99952f
Helder Oliveira <heldrida@gmail.com>
2022-03-21 16:54:32 +0000

    fix: 🐛 use the crowns test canister id

--- .scripts/mock/kyasshu-cache-tokens.sh
+++ .scripts/mock/kyasshu-cache-tokens.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-_marketplaceId=$(cd nft-marketplace && dfx canister id marketplace)
+# At time of writing we're pulling data from the Crowns test canister
+# that has the id "iqvo2-7qaaa-aaaam-qacxa-cai"
+# https://github.com/Psychedelic/crowns/blob/main/canister_ids.json
+_crownsCanisterId="iqvo2-7qaaa-aaaam-qacxa-cai"
 _count=$1
 
 printf "🤖 Kyasshu Cache tokens\n"
 printf "Count is set to (%s)\n" "$_count"
-printf "The marketplace id (%s)\n" "$_marketplaceId"
+printf "The Crowns Canister id (%s)\n" "$_crownsCanisterId"
 
 curl -X GET \
-  http://localhost:3000/dev/marketplace/"$_marketplaceId"/cache/"$_count"
+  http://localhost:3000/local/marketplace/"$_crownsCanisterId"/cache/"$_count"
