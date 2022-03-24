74f2f9e525066604d40165e428311be1b10c394f
ossian <self@ossian.dev>
2022-03-24 08:57:03 -0400

    feat: emoji!

--- .scripts/kyasshu/start.sh
+++ .scripts/kyasshu/start.sh
@@ -4,7 +4,7 @@ CAP_ID=$(cd nft-marketplace/cap && dfx canister id ic-history-router)
 NFT_CANISTER_ID=$(cd nft-marketplace/crowns && dfx canister id crowns)
 NFT_CANISTER_STANDARD='DIP721v2'
 
-printf "Starting kyasshu with the local services cluster...\n\n"
+printf "🤖 Starting kyasshu with the local services cluster...\n\n"
 printf "  Using CAP_ID: $CAP_ID\n"
 printf "  Using NFT_CANISTER_ID: $NFT_CANISTER_ID\n"
 printf "  Using NFT_CANISTER_STANDARD: $NFT_CANISTER_STANDARD\n\n"
