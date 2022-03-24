b97ca950de52c54a468868232f737bf4b9df63be
ossian <self@ossian.dev>
2022-03-24 09:32:12 -0400

    chore: require local or mainnet specification in kyasshu:start

--- .scripts/kyasshu/start.sh
+++ .scripts/kyasshu/start.sh
@@ -2,16 +2,16 @@
 
 host=$1
 
-if [[ -z $host || "$host" == "local" ]]; then
+if [[ "$host" == "local" ]]; then
   CAP_ID=$(cd nft-marketplace/cap && dfx canister id ic-history-router)
   NFT_CANISTER_ID=$(cd nft-marketplace/crowns && dfx canister id crowns)
   NFT_CANISTER_STANDARD='DIP721v2'
-elif [[ "$1" != 'mainnet' ]]; then
+elif [[ "$host" != 'mainnet' ]]; then
   printf "usage: yarn kyasshu:start [service cluster: local | mainnet]\n"
   exit 1
 fi
 
-printf "🤖 Starting kyasshu with the $host services cluster...\n\n"
+printf "🤖 Starting kyasshu with the $host services...\n\n"
 
 [[ ! -z $CAP_ID ]] && printf "  Using CAP_ID: $CAP_ID\n"
 [[ ! -z $NFT_CANISTER_ID ]] && printf "  Using NFT_CANISTER_ID: $NFT_CANISTER_ID\n"
