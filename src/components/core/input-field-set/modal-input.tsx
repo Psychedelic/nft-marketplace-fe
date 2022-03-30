59a582b0e106a947bc59504d8bd111c2aa6a374b
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-03-30 12:02:59 +0530

    feat: added default value to modal input filed

--- src/components/core/input-field-set/modal-input.tsx
+++ src/components/core/input-field-set/modal-input.tsx
@@ -12,35 +12,40 @@ import {
 export type ModalInputProps = {
   placeholder?: string;
   setValue?: (value: string) => void;
+  defaultValue?: string;
 };
 
-export const ModalInput = forwardRef<HTMLInputElement, ModalInputProps>(
-  ({ placeholder = '', setValue }, ref) => {
-    const { t } = useTranslation();
+export const ModalInput = forwardRef<
+  HTMLInputElement,
+  ModalInputProps
+>(({ placeholder = '', setValue, defaultValue = '' }, ref) => {
+  const { t } = useTranslation();
 
-    const handleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
-      const value = event?.target?.value;
-      if (setValue) {
-        setValue(value);
-      }
-    };
+  const handleValueChange = (
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = event?.target?.value;
+    if (setValue) {
+      setValue(value);
+    }
+  };
 
-    return (
-      <Container name="modalInput">
-        <Input
-          ref={ref}
-          name="modalInput"
-          type="number"
-          placeholder={placeholder}
-          onChange={handleValueChange}
-        />
-        <AmountTypeContainer>
-          <AmountTypeIcon src={wicpLogo} alt="WICP" />
-          <AmountTypeTitle>
-            {t('translation:inputField.amountTypes.wicp')}
-          </AmountTypeTitle>
-        </AmountTypeContainer>
-      </Container>
-    );
-  },
-);
+  return (
+    <Container name="modalInput">
+      <Input
+        ref={ref}
+        name="modalInput"
+        type="number"
+        placeholder={placeholder}
+        onChange={handleValueChange}
+        defaultValue={defaultValue}
+      />
+      <AmountTypeContainer>
+        <AmountTypeIcon src={wicpLogo} alt="WICP" />
+        <AmountTypeTitle>
+          {t('translation:inputField.amountTypes.wicp')}
+        </AmountTypeTitle>
+      </AmountTypeContainer>
+    </Container>
+  );
+});
