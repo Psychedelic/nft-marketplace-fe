e99c30f1f953b428be87330ce3e6e692bf745422
prasanth lalapeta <sonoffrontend.developer@gmail.com>
2022-04-08 10:54:46 +0530

    feat: added mock offers table data

---
+++ src/components/tables/mock-offers-data.ts
@@ -0,0 +1,12 @@
+import crownsLogo from '../../assets/crowns-logo.svg';
+
+// TODO: Update offers mock data table
+
+export const mockTableData = [
+  {
+    price: '5',
+    floorDifference: '-',
+    expiration: '-',
+    offerFrom: 'ffuck...cae',
+  },
+];
