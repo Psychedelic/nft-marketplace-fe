83870baddb5a661e9e84e2404e0b88857379eb75
prasanth kumar lalapeta <40259256+prasanthLalapeta@users.noreply.github.com>
2022-05-03 19:58:29 +0530

    feat: 🎸 offers made integration (#144)
    * todos: added todos to add offers made integrations
    
    * feat: configured getBuyeroffers marketplace API
    
    * feat: added parser to parse offers made response
    
    * feat: added getBuyerOffers API integration in my-offers-table
    
    * refactor: removed todo's after finishing API integration
    
    Co-authored-by: Prasanth Lalapeta <prasanth@Prasanths-MacBook-Pro.local>

--- src/store/features/marketplace/marketplace-slice.ts
+++ src/store/features/marketplace/marketplace-slice.ts
@@ -15,7 +15,7 @@ import { RootState } from '../../store';
 import { OwnerTokenIdentifiers } from '../../features/crowns/crowns-slice';
 // import { crownsSlice } from '../crowns/crowns-slice';
 // import { wicpSlice } from '../wicp/wicp-slice';
-import { parseGetTokenOffersresponse } from '../../../utils/parser';
+import { parseGetTokenOffersresponse, parseOffersMaderesponse } from '../../../utils/parser';
 import { getICPPrice } from '../../../integrations/marketplace/price.utils';
 
 interface MakeListingParams extends MakeListing {
@@ -75,6 +75,15 @@ interface GetUserReceviedOfferParams extends GetUserReceviedOffer {
 
 type GetUserReceviedOffer = {
   ownerTokenIdentifiers?: OwnerTokenIdentifiers,
+};
+
+interface GetBuyerOffersParams extends GetBuyerOffers {
+  onSuccess?: (offers: any) => void;
+  onFailure?: () => void;
+}
+
+type GetBuyerOffers = {
+  userPrincipalId: string,
 };
 
 type RecentyListedForSale = MakeListing[];
@@ -493,6 +502,65 @@ export const getTokenOffers = createAsyncThunk<
     }
 
     const parsedTokenOffers = parseGetTokenOffersresponse({
+      data: result,
+      floorDifferencePrice,
+      currencyMarketPrice
+    });
+
+    if (!Array.isArray(result) || !result.length) return [];
+
+    if (typeof onSuccess !== 'function') return;
+
+    onSuccess(parsedTokenOffers);
+  } catch (err) {
+    thunkAPI.dispatch(notificationActions.setErrorMessage((err as CommonError).message));
+    if (typeof onFailure !== 'function') return;
+    onFailure();
+  }
+});
+
+export const getBuyerOffers = createAsyncThunk<
+  // Return type of the payload creator
+  // GetBuyerOffers | undefined,
+  any | undefined,
+  // First argument to the payload creator
+  GetBuyerOffersParams,
+  // Optional fields for defining the thunk api
+  { state: RootState }
+>('marketplace/getBuyerOffers', async (params: GetBuyerOffersParams, thunkAPI) => {
+  // Checks if an actor instance exists already
+  // otherwise creates a new instance
+  const actorInstance = await actorInstanceHandler({
+    thunkAPI,
+    serviceName: 'marketplace',
+    slice: marketplaceSlice,
+  });
+
+  const { userPrincipalId, onSuccess, onFailure } = params;
+
+  try {
+    let floorDifferencePrice;
+    let currencyMarketPrice = undefined;
+    const nonFungibleContractAddress = Principal.fromText(config.crownsCanisterId);
+    const userPrincipalAddress = Principal.fromText(userPrincipalId);
+    const result = await actorInstance.getBuyerOffers(
+      nonFungibleContractAddress,
+      userPrincipalAddress,
+      );
+
+    // Floor Difference calculation
+    const floorDifferenceResponse = await actorInstance.getFloor(nonFungibleContractAddress);
+    if (('Ok' in floorDifferenceResponse)) {
+      floorDifferencePrice = floorDifferenceResponse.Ok.toString();
+    }
+
+    // Fetch ICP Price
+    const icpPriceResponse = await getICPPrice();
+    if (icpPriceResponse && icpPriceResponse.usd) {
+      currencyMarketPrice = icpPriceResponse.usd;
+    }
+
+    const parsedTokenOffers = parseOffersMaderesponse({
       data: result,
       floorDifferencePrice,
       currencyMarketPrice
