b4db3bcbc0617e90a6178355e549bb7a50db82ee
Felipe Mendes <zo.fmendes@gmail.com>
2022-05-13 09:06:16 -0300

    feat: 🎸 tooltip for nft card numbers (#205)
    feat: add number tooltip

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -49,3 +49,50 @@ export const parseE8SAmountToWICP = (amount: bigint) => {
 
   return computedWICP.toString();
 };
+
+const fixStringEnding = (str: string): string =>
+  str.replace(/0+$/, '').replace(/\.$/, '');
+
+export const formatAmountDecimals = (amount?: string | number) => {
+  const fixedAmount = Number(amount).toFixed(8);
+  if (fixedAmount === 'NaN' || fixedAmount === 'Infinity' || !amount)
+    return '0.00';
+
+  return fixStringEnding(fixedAmount);
+};
+
+export const formatAmountAbbreviation = (
+  amount?: string | number,
+): string => {
+  const fixedAmount = Number(amount).toFixed(8);
+  if (fixedAmount === 'NaN' || fixedAmount === 'Infinity' || !amount)
+    return '0.00';
+
+  const [nat = '0', decimals = '0'] = fixedAmount
+    .replace(/^0+/, '')
+    .split('.');
+
+  if (Number(nat) === 0 && Number(decimals) === 0) return '0.00';
+
+  const isNegative = Math.sign(Number(fixedAmount)) === -1;
+
+  const thousands = Math.floor(Math.log10(Math.abs(Number(nat))));
+
+  if (thousands < 3) {
+    if (!Number(nat) && /^00/.test(decimals)) {
+      return `${isNegative ? '> -' : '< '}0.01`;
+    }
+    return fixStringEnding(`${nat || 0}.${decimals.slice(0, 2)}`);
+  }
+  if (thousands < 6) {
+    return `${fixStringEnding(
+      `${nat.slice(0, -3)}.${nat.slice(-3, -1)}`,
+    )}k`;
+  }
+  if (thousands < 9) {
+    return `${fixStringEnding(
+      `${nat.slice(0, -6)}.${nat.slice(-6, -4)}`,
+    )}M`;
+  }
+  return `${isNegative ? '< -' : '> '}999M`;
+};
