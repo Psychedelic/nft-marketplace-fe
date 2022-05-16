b759aa7732b3955fd98b6cfcaaa02110bab53ea6
Felipe Mendes <zo.fmendes@gmail.com>
2022-05-16 16:56:50 -0300

    fix: add animation to traits skeleton

--- src/components/nft-details/TraitsListLoader.tsx
+++ src/components/nft-details/TraitsListLoader.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { TraitsListLoaderSkeletonWrapper, NFTTraitsChipSkeleton } from './styles';
+import {
+  TraitsListLoaderSkeletonWrapper,
+  NFTTraitsChipSkeleton,
+} from './styles';
 
-const TraitsListLoader = () => {
-  return (
-    <TraitsListLoaderSkeletonWrapper>
-      <NFTTraitsChipSkeleton></NFTTraitsChipSkeleton>
-      <NFTTraitsChipSkeleton></NFTTraitsChipSkeleton>
-      <NFTTraitsChipSkeleton></NFTTraitsChipSkeleton>
-      <NFTTraitsChipSkeleton></NFTTraitsChipSkeleton>
-    </TraitsListLoaderSkeletonWrapper>
-  );
-};
+const TraitsListLoader = () => (
+  <TraitsListLoaderSkeletonWrapper>
+    <NFTTraitsChipSkeleton />
+    <NFTTraitsChipSkeleton />
+    <NFTTraitsChipSkeleton />
+    <NFTTraitsChipSkeleton />
+  </TraitsListLoaderSkeletonWrapper>
+);
 
 export default TraitsListLoader;
+
