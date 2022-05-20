e46ab1f0d88bbdb3e22960ba2460e005cf70bd45
prasanth kumar lalapeta <40259256+prasanthLalapeta@users.noreply.github.com>
2022-05-20 19:07:09 +0530

    fix: 🐛 NFT card hover issue (#274)
    * chore: added default value of isPreview to false
    
    * refactor: removed unnecessary setModalStatus
    
    * refactor: removed unnecessary onClose

--- src/components/core/cards/nft-card/nft-card.tsx
+++ src/components/core/cards/nft-card/nft-card.tsx
@@ -46,12 +46,10 @@ type ConnectedProps = {
   isForSale?: boolean;
   tokenId: string;
   price?: bigint;
-  setModalStatus: (status: boolean) => void;
 };
 
 type DisconnectedProps = {
   isForSale?: boolean;
-  setModalStatus: (status: boolean) => void;
 };
 
 type LastActionTakenDetailsProps = {
@@ -65,7 +63,6 @@ const OnConnected = ({
   isForSale,
   tokenId,
   price,
-  setModalStatus,
 }: ConnectedProps) => {
   const { t } = useTranslation();
   const showBuyerOptions = !owned;
@@ -74,16 +71,14 @@ const OnConnected = ({
   return (
     <>
       {showSellOptions && (
-        <div onClick={() => setModalStatus(true)} role="dialog">
+        <div role="dialog">
           <SellModal
-            onClose={() => setModalStatus(false)}
             actionText={`${t('translation:nftCard.sell')}`}
             nftTokenId={tokenId}
             isTriggerVisible={!isForSale}
           />
 
           <ChangePriceModal
-            onClose={() => setModalStatus(false)}
             actionText={`${t('translation:nftCard.changePrice')}`}
             nftTokenId={tokenId}
             nftPrice={price}
@@ -92,9 +87,8 @@ const OnConnected = ({
         </div>
       )}
       {(showBuyerOptions && (
-        <div onClick={() => setModalStatus(true)} role="dialog">
+        <div role="dialog">
           <BuyNowModal
-            onClose={() => setModalStatus(false)}
             actionText={`${t('translation:nftCard.forSale')}`}
             actionTextId={Number(tokenId)}
             price={
@@ -103,7 +97,6 @@ const OnConnected = ({
             isTriggerVisible={isForSale}
           />
           <MakeOfferModal
-            onClose={() => setModalStatus(false)}
             actionText={`${t('translation:nftCard.forOffer')}`}
             nftTokenId={tokenId}
             isTriggerVisible={!isForSale}
@@ -114,13 +107,10 @@ const OnConnected = ({
   );
 };
 
-const OnDisconnected = ({
-  isForSale,
-  setModalStatus,
-}: DisconnectedProps) => {
+const OnDisconnected = ({ isForSale }: DisconnectedProps) => {
   const { t } = useTranslation();
   return (
-    <div onClick={() => setModalStatus(true)} role="dialog">
+    <div role="dialog">
       {isForSale ? (
         <ConnectToPlugModal
           actionText={`${t('translation:nftCard.forSale')}`}
@@ -184,22 +174,17 @@ const LastActionTakenDetails = ({
 };
 
 export const NftCard = React.memo(
-  ({ owned, data, previewCard }: NftCardProps) => {
+  ({ owned, data, previewCard = false }: NftCardProps) => {
     const { t } = useTranslation();
-    const [modalOpen, setModalOpen] = useState(false);
     const { isConnected } = usePlugStore();
     const containerRef = useRef<HTMLDivElement>(null);
 
     // TODO: Move any status code as constant
     const isForSale = data.status === 'forSale';
 
-    const setModalStatus = (status: boolean) => {
-      setModalOpen(status);
-    };
-
     return (
       <CardContainer
-        disableAnimation={modalOpen || previewCard}
+        disableAnimation={previewCard}
         ref={containerRef}
       >
         <CardWrapper>
@@ -253,15 +238,9 @@ export const NftCard = React.memo(
                   owned={owned}
                   isForSale={isForSale}
                   tokenId={data.id}
-                  setModalStatus={setModalStatus}
                   price={data?.price}
                 />
-              )) || (
-                <OnDisconnected
-                  isForSale={isForSale}
-                  setModalStatus={setModalStatus}
-                />
-              )}
+              )) || <OnDisconnected isForSale={isForSale} />}
               <LastActionTakenDetails
                 data={data}
                 isForSale={isForSale}
