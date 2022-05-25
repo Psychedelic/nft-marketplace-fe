f7172c5f2c1b0fd8f16cfc60a34e69a7aea3b137
Felipe Mendes <zo.fmendes@gmail.com>
2022-05-25 10:40:17 -0300

    feat: add video and image preload components

---
+++ src/components/image-preload/image-preload.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import React, { forwardRef, useMemo, useState } from 'react';
+import { ImageCache } from '../../utils/image-cache';
+import { AppLog } from '../../utils/log';
+import { SkeletonBox } from '../core';
+import { ImagePreloadStyles } from './styles';
+
+export type ImagePreloadProps = React.ComponentProps<
+  typeof ImagePreloadStyles
+>;
+
+export const ImagePreload = forwardRef<
+  HTMLImageElement,
+  ImagePreloadProps
+>(({ src, ...props }, ref) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useMemo(() => {
+    if (src) {
+      const image = ImageCache.get(src);
+
+      if (!image) {
+        ImageCache.store(src)
+          .then(() => setLoaded(true))
+          .catch((err) => AppLog.warn('Failed to load image', err));
+      } else {
+        setLoaded(true);
+      }
+    }
+  }, [src, setLoaded]);
+
+  if (!loaded) {
+    return (
+      <SkeletonBox
+        style={props.style}
+        className={props.className}
+        ref={ref}
+      />
+    );
+  }
+
+  return <ImagePreloadStyles src={src} {...props} ref={ref} />;
+});
