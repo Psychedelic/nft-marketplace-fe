fd927f4e36ac4f8bf3358b6198cde08cd9e7fa7c
prasanth kumar lalapeta <40259256+prasanthLalapeta@users.noreply.github.com>
2022-06-06 17:58:05 +0530

    fix: 🐛 remove cancel button from buy now modal (#354)
    fix/removed cancel button from buy now modal

--- src/components/modals/buy-now-modal.tsx
+++ src/components/modals/buy-now-modal.tsx
@@ -194,21 +194,6 @@ export const BuyNowModal = ({
               ---------------------------------
             */}
               <Pending />
-              {/*
-              ---------------------------------
-              Pending Action Buttons
-              ---------------------------------
-            */}
-              <ModalButtonsList>
-                <ModalButtonWrapper fullWidth>
-                  <ActionButton
-                    type="secondary"
-                    onClick={handleModalClose}
-                  >
-                    {t('translation:modals.buttons.cancel')}
-                  </ActionButton>
-                </ModalButtonWrapper>
-              </ModalButtonsList>
             </Container>
           )}
           {/*
