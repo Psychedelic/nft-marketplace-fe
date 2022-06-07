d3809575993b11e38e3ea34e1908be6bb723227f
Oyinlolu <oyinloluwab@gmail.com>
2022-06-07 13:09:13 +0100

    refactor: 💡 refactored sortTransactionsByTime function

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,6 +1,8 @@
 import { OffersTableItem } from '../declarations/legacy';
 import { ParsedTokenOffers, TokenTransactionItem } from './parser';
 
+type TnxTimestamp = TokenTransactionItem | OffersTableItem;
+
 export const sortTokenOffersByPrice = (offers: ParsedTokenOffers) => {
   return offers.sort((a, b) => {
     const offerAPrice = Number(a.price.toString());
@@ -14,11 +16,9 @@ export const sortTokenOffersByPrice = (offers: ParsedTokenOffers) => {
 export const sortTransactionsByTime = (
   transactions: TokenTransactionItem[] | OffersTableItem[],
 ) => {
-  return transactions.sort((a: any, b: any) => {
-    const transactionATimestamp = a.time;
-    const transactionBTimestamp = b.time;
-
-    // sort type: Ascending
-    return transactionBTimestamp - transactionATimestamp;
-  });
+  return transactions.sort(
+    (a: TnxTimestamp, b: TnxTimestamp) => {
+      return Number(b.time.toString()) - Number(a.time.toString())
+    }
+  );
 };
