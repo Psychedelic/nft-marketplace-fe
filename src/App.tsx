ccd76f13e4a2f9a45ce6308541c0e8f9964739c6
prasanthLalapeta <sonoffrontend.developer@gmail.com>
2022-06-24 12:26:35 +0530

    feat: added my activity button in plug dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { NavBar, ToastHandler, Alerts } from './components';
 import CollectionView from './views/CollectionView';
 import NFTView from './views/NFTView';
 import OfferView from './views/OffersView';
+import UserActivityView from './views/UserActivityView';
 import { useTheme } from './hooks/use-theme';
 import { portalZIndexGlobals } from './utils/styles';
 import { ThemeRootElement } from './constants/common';
@@ -26,6 +27,10 @@ const App = () => {
           </Route>
           <Route path="/nft/:id" element={<NFTView />} />
           <Route path="/offers/:id" element={<OfferView />} />
+          <Route
+            path="/activity/:id"
+            element={<UserActivityView />}
+          />
         </Routes>
       </BrowserRouter>
       <ToastHandler />
