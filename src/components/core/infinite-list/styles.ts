17615e4c78ba7f5fb0e3bdb35bc84ca844e7c091
Oyinlolu <oyinloluwab@gmail.com>
2022-07-15 00:56:18 +0100

    refactor: 💡 modified mobile responsive nft listing

--- src/components/core/infinite-list/styles.ts
+++ src/components/core/infinite-list/styles.ts
@@ -1,13 +1,7 @@
 import { styled } from '../../../stitches.config';
 
 export const CardListContainer = styled('div', {
-  width: '450px',
-
   '@large': {
     width: '100%',
-  },
-
-  '@sm': {
-    width: '390px',
   },
 });
