4117cf830822e82b858f5dfc15a87e8142b3ebe8
prasanthLalapeta <sonoffrontend.developer@gmail.com>
2022-08-26 14:21:31 +0530

    refactor: added mock collections list

--- src/components/search/mock-data.ts
+++ src/components/search/mock-data.ts
@@ -1,39 +1,12 @@
 import crownsLogo from '../../assets/crowns-logo.svg';
 
-export const mockNFTList = [
+export const mockCollectionsList = [
   {
-    id: '2700',
-    name: 'CAP Crowns #2700',
-    wicp: '5.12 WICP',
-    price: '$220.00',
+    id: '1',
+    name: 'CAP Crowns',
     logo: crownsLogo,
-  },
-  {
-    id: '2701',
-    name: 'CAP Crowns #2701',
-    wicp: '6.12 WICP',
-    price: '$320.93',
-    logo: crownsLogo,
-  },
-  {
-    id: '2702',
-    name: 'CAP Crowns #2702',
-    wicp: '7.12 WICP',
-    price: '$420.00',
-    logo: crownsLogo,
-  },
-  {
-    id: '2703',
-    name: 'CAP Crowns #2703',
-    wicp: '5.12 WICP',
-    price: '$220.67',
-    logo: crownsLogo,
-  },
-  {
-    id: '2704',
-    name: 'CAP Crowns #2704',
-    wicp: '4.12 WICP',
-    price: '$120.00',
-    logo: crownsLogo,
+    creator: 'Psychedelic',
+    items: '10,000',
+    volume: '33k',
   },
 ];
