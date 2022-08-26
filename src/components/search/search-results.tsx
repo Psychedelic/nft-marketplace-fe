3d31ac9f1bfebd2146a5b20eb42b06e6eaacde6f
prasanthLalapeta <sonoffrontend.developer@gmail.com>
2022-08-26 11:52:42 +0530

    refactor: moved NFT search results to seperate component naming NFT-search-results

--- src/components/search/search-results.tsx
+++ src/components/search/search-results.tsx
@@ -1,27 +1,5 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
-import {
-  ItemsEmptyContainer,
-  ItemsListContainer,
-  ItemDetailsWrapper,
-  ItemDetails,
-  ItemLogo,
-  ItemName,
-  PriceDetails,
-  WICPContainer,
-  WICPText,
-  WICPLogo,
-  PriceText,
-  SubText,
-  LoadingWrapper,
-  ItemDetailsContainer,
-  StyledRouterLink,
-} from './styles';
-import { useFilterStore } from '../../store';
-import { formatPriceValue } from '../../utils/formatters';
-import wicpIcon from '../../assets/wicp.svg';
-import { SpinnerIcon } from '../icons/custom';
-import config from '../../config/env';
+import NFTsSearchResults from './nfts-search-results';
 
 type SearchResultsTypes = {
   searchText: string;
@@ -33,65 +11,13 @@ const SearchResults = ({
   closeDropDown,
 }: SearchResultsTypes) => {
   const { t } = useTranslation();
-  const { searchResults, loadingSearch } = useFilterStore();
 
   return (
     <>
-      {searchText &&
-        !loadingSearch &&
-        (searchResults.length ? (
-          <ItemsListContainer>
-            {searchResults?.map((nft) => (
-              <StyledRouterLink
-                to={`/${config.nftCollectionId}/nft/${nft.id}`}
-                onClick={closeDropDown}
-                key={nft.id}
-              >
-                <ItemDetailsContainer>
-                  <ItemDetailsWrapper>
-                    <ItemDetails>
-                      <ItemLogo src={nft.preview} alt="crowns" />
-                      <ItemName>{`${nft.name} ${nft.id}`}</ItemName>
-                    </ItemDetails>
-                    <PriceDetails>
-                      {Boolean(nft?.wicpPrice) && (
-                        <WICPContainer size="small">
-                          <WICPLogo src={wicpIcon} alt="wicp" />
-                          <WICPText size="small">
-                            {nft.wicpPrice}
-                            WICP
-                          </WICPText>
-                        </WICPContainer>
-                      )}
-                      {Boolean(nft?.price) && (
-                        <PriceText>
-                          <SubText>$</SubText>
-                          <SubText>{`${formatPriceValue(
-                            nft.price.toString(),
-                          )}`}</SubText>
-                        </PriceText>
-                      )}
-                    </PriceDetails>
-                  </ItemDetailsWrapper>
-                </ItemDetailsContainer>
-              </StyledRouterLink>
-            ))}
-          </ItemsListContainer>
-        ) : (
-          <ItemsEmptyContainer>
-            {t('translation:emptyStates.noNFTId')}
-          </ItemsEmptyContainer>
-        ))}
-      {!searchText && !loadingSearch && (
-        <ItemsEmptyContainer>
-          {t('translation:common.noRecentSearch')}
-        </ItemsEmptyContainer>
-      )}
-      {loadingSearch && (
-        <LoadingWrapper>
-          <SpinnerIcon />
-        </LoadingWrapper>
-      )}
+      <NFTsSearchResults
+        searchText={searchText}
+        closeDropDown={closeDropDown}
+      />
     </>
   );
 };
