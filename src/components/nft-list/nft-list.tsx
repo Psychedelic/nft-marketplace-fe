782e04a1b0833dfe0f9654ca01c9ae7c2b42f929
prasanthLalapeta <sonoffrontend.developer@gmail.com>
2022-09-26 12:55:49 +0530

    fix: updated loadmore logic in nft list component to fix pagination

--- src/components/nft-list/nft-list.tsx
+++ src/components/nft-list/nft-list.tsx
@@ -69,8 +69,7 @@ export const NftList = () => {
       loadingNFTs ||
       !hasMoreNFTs ||
       nextPageNo <= 0 ||
-      !collectionId ||
-      !isMyNfts
+      !collectionId
     )
       return;
 
