bf325fd9598772c3bd80d772ecdc4ea646d7b339
prasanthLalapeta <sonoffrontend.developer@gmail.com>
2022-10-13 10:30:06 +0530

    chore: added verify ICNS or Crowns collection utility methods

---
+++ src/utils/collections.ts
@@ -0,0 +1,15 @@
+import { collectionNames } from '../constants/collection-names';
+
+export const isICNSCollection = (collectionName: string) => {
+  if (!collectionName) return;
+
+  return collectionName.toLowerCase().includes(collectionNames.icns);
+};
+
+export const isCrownsCollection = (collectionName: string) => {
+  if (!collectionName) return;
+
+  return collectionName
+    .toLowerCase()
+    .includes(collectionNames.crowns);
+};
